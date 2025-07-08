1d3e82bf9068063107f568dd78c57b493e4a3cd9
ryugasiria <vishnu.r871043@gmail.com>
2025-07-08 21:05:11 +0530

    Fix milestone reward null access error

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -188,9 +188,9 @@ const Dashboard: React.FC = () => {
             <div className="flex items-center justify-center space-x-4 text-white">
               <div className="flex items-center">
                 <Coins className="w-5 h-5 mr-1" />
-                <span>+{user.milestones.find(m => m.id === showMilestoneReward)?.reward.coins}</span>
+                <span>+{user.milestones.find(m => m.id === showMilestoneReward)?.reward?.coins || 0}</span>
               </div>
-              {user.milestones.find(m => m.id === showMilestoneReward)?.reward.items && (
+              {user.milestones.find(m => m.id === showMilestoneReward)?.reward?.items && (
                 <div className="text-sm">+ Special Items</div>
               )}
             </div>
@@ -351,8 +351,8 @@ const Dashboard: React.FC = () => {
             showText={false}
           />
           <div className="flex items-center justify-between mt-2 text-xs text-gray-400">
-            <span>Reward: {nextMilestone.reward.coins} coins</span>
-            {nextMilestone.reward.items && <span>+ Special Items</span>}
+            <span>Reward: {nextMilestone.reward?.coins || 0} coins</span>
+            {nextMilestone.reward?.items && <span>+ Special Items</span>}
           </div>
         </div>
       )}
